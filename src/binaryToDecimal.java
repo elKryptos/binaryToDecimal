58dbe002436303c732d91bf1266f0e2aa87d4e0f
elKryptos <hans_09@hotmail.it>
2023-12-18 23:52:24 +0100

    conversione decimal

--- src/binaryToDecimal.java
+++ src/binaryToDecimal.java
@@ -5,10 +5,22 @@ public class binaryToDecimal {
         System.out.println("Inserisci una stringa binaria: ");
         String binaryString = scanner.nextLine();
 
-        int decimalValue = 0;
-        //int decimalValue = binaryToDecimal (binaryString);
+        //int decimalValue = 0;
+        int decimalValue = binaryToDecimal (binaryString);
 
         System.out.println("Valore decimale: " + decimalValue);
         scanner.close();
+    }
+
+    public static int binaryToDecimal(String binaryString){
+        int decimalValue = 0;
+        int len = binaryString.length();
+
+        for (int i  = 0; i < len; i++){
+            int digit = Character.getNumericValue(binaryString.charAt(i));
+            int power = len - i - 1;
+            decimalValue = decimalValue + (int)(digit * Math.pow(3,power));
+        }
+        return decimalValue;
     }
 }
