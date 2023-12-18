b28e388712e080ce2c7993cb44fb57e241738d37
elKryptos <hans_09@hotmail.it>
2023-12-18 23:36:40 +0100

    conversione

---
+++ src/binaryToDecimal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class binaryToDecimal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Inserisci una stringa binaria: ");
+        String binaryString = scanner.nextLine();
+
+        int decimalValue = 0;
+        //int decimalValue = binaryToDecimal (binaryString);
+
+        System.out.println("Valore decimale: " + decimalValue);
+        scanner.close();
+    }
+}
